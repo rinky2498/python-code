18994b65022dabf8efb32cc91f0374686a5f5f1a
Rinky <rinkygarg2498@gmail.com>
2021-03-17 00:17:03 +0530

    lessthsn_5

---
+++ lessthan_5.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+##program that prints out all the elements of the list that are less than 5.
+thislist = [1,1,2,3,5,8,13,21,34,55,89]
+newlist = []
+for item in thislist:
+    
+    if item < 5:
+        
+newlist.append(item)
+    
+print(newlist)
+
+
+
